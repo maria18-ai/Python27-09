a0b7783a61ff32d9449583392249a0981ee70722
Maria E. T. Fernandes <131560480+maria18-ai@users.noreply.github.com>
2023-09-29 15:17:40 -0300

    Update atividade4.py

--- python29-09/atividade4.py
+++ python29-09/atividade4.py
@@ -1,9 +1,8 @@
 // Programa para mostrar a soma de todos os números pares de uma lista
 num = [3,6,23,12,7,5,2,];
 
-soma_pares = sum(x for x in num if x % 2 == 0)
+soma_pares = sum(num[::2])
 
-print(soma_pares)
 print("A soma dos números pares é: ",soma_pares)
 
 
