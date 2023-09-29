3ee43c8073b7be733729a4381936ab87d8242494
Maria E. T. Fernandes <131560480+maria18-ai@users.noreply.github.com>
2023-09-29 15:09:52 -0300

    Update atividade8.py

--- python29-09/python-29-09/atividade8.py
+++ python29-09/python-29-09/atividade8.py
@@ -1,3 +1,5 @@
+//Programa para imprimir uma tupla com os signos dos zodiacos 
+
 zodiacos = ("Áries", "Touro", "Gêmeos", "Câncer", "Leão", "Virgem", 
           "Libra", "Escorpião", "Ofiúco", "Sagitário", "Capricórnio", "Aquário", "Peixes");
 
